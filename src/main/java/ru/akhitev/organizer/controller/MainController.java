69c9f29042f19eb2e63545ce6d226e3ceeec9834
Aleksei Khitev <aleksei_khitev@epam.com>
2017-11-16 11:31:51 +0300

    javafx main controller

---
+++ src/main/java/ru/akhitev/organizer/controller/MainController.java
@@ -0,0 +1,13 @@
+package ru.akhitev.organizer.controller;
+
+import de.felixroske.jfxsupport.FXMLController;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+@FXMLController
+public class MainController {
+
+    @FXML
+    private Label helloLabel;
+
+}
