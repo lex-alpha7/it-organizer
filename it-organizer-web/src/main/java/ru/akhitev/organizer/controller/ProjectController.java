9ddad6dae26b9b447db0da760860eed97b854087
Aleksei Khitev <aleksei_khitev@epam.com>
2018-03-29 09:12:16 +0300

    Save project

--- it-organizer-web/src/main/java/ru/akhitev/organizer/controller/ProjectController.java
+++ it-organizer-web/src/main/java/ru/akhitev/organizer/controller/ProjectController.java
@@ -3,10 +3,9 @@ package ru.akhitev.organizer.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.akhitev.organizer.entity.Project;
 import ru.akhitev.organizer.repository.ProjectRepository;
 
 import java.util.Map;
@@ -19,6 +18,13 @@ public class ProjectController {
 
     @RequestMapping(value = "/new", method = RequestMethod.GET)
     public String newProject(Model model) {
+        model.addAttribute("project", new Project());
         return "fragments/project :: new";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveProject(@ModelAttribute Project project, BindingResult bindingResult, Model model) {
+        projectRepository.save(project);
+        return "redirect:/";
     }
 }
