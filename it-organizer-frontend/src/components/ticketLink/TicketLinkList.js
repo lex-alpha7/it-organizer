740e088280eb4683df96170dcd847acf95bbbd4b
Aleksei Khitev <alexkhitev@gmail.com>
2020-04-22 16:06:57 +0300

    made ticket links in more convenient way

--- it-organizer-frontend/src/components/ticketLink/TicketLinkList.js
+++ it-organizer-frontend/src/components/ticketLink/TicketLinkList.js
@@ -4,30 +4,82 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFileMedical } from '@fortawesome/free-solid-svg-icons'
 
 const TicketLinkList = (props) => {
-    let element_html = "";
+    let linksToTickets = "";
+    let useFullLinks = "";
+    let attachments = "";
     if (props.ticketLinks) {
-        element_html = props.ticketLinks.map(
+        linksToTickets = props.ticketLinks.map(
             function(ticketLink) {
-                let key = 'ticketLinkid' + ticketLink.id;
-                return <TicketLinkListItem key={key} ticketLink={ticketLink}
-                    saveTicket={props.saveTicket}
-                    updateTicket={props.updateTicket}
-                    ticket={props.ticket}
-                    showSuccessAlert={props.showSuccessAlert}
-                    showErrorAlert={props.showErrorAlert}/>;
+                if (ticketLink.type == 'LINK_TO_TICKET') {
+                    let key = 'ticketLinkid' + ticketLink.id;
+                    return <TicketLinkListItem key={key} ticketLink={ticketLink}
+                        saveTicket={props.saveTicket}
+                        updateTicket={props.updateTicket}
+                        ticket={props.ticket}
+                        showSuccessAlert={props.showSuccessAlert}
+                        showErrorAlert={props.showErrorAlert}/>;
+                } else {
+                    return "";
+                }
+            }
+        );
+        useFullLinks = props.ticketLinks.map(
+            function(ticketLink) {
+                if (ticketLink.type == 'USEFUL_LINK') {
+                    let key = 'ticketLinkid' + ticketLink.id;
+                    return <TicketLinkListItem key={key} ticketLink={ticketLink}
+                        saveTicket={props.saveTicket}
+                        updateTicket={props.updateTicket}
+                        ticket={props.ticket}
+                        showSuccessAlert={props.showSuccessAlert}
+                        showErrorAlert={props.showErrorAlert}/>;
+                } else {
+                    return "";
+                }
+            }
+        );
+        attachments = props.ticketLinks.map(
+            function(ticketLink) {
+                if (ticketLink.type == 'ATTACHMENT') {
+                    let key = 'ticketLinkid' + ticketLink.id;
+                    return <TicketLinkListItem key={key} ticketLink={ticketLink}
+                        saveTicket={props.saveTicket}
+                        updateTicket={props.updateTicket}
+                        ticket={props.ticket}
+                        showSuccessAlert={props.showSuccessAlert}
+                        showErrorAlert={props.showErrorAlert}/>;
+                } else {
+                    return "";
+                }
             }
         );
     }
     return(
         <div>
-          { element_html &&
-                          <div className="btn-group">
-                            <button type="button" className="btn btn-secondary dropdown-toggle"  data-toggle="dropdown">Links</button>
-                            <div className="dropdown-menu">
-                              {element_html}
-                            </div>
-                          </div>
-                      }
+          { linksToTickets &&
+              <div className="btn-group">
+                <button type="button" className="btn btn-outline-primary dropdown-toggle"  data-toggle="dropdown">Links to Tickets</button>
+                <div className="dropdown-menu">
+                  {linksToTickets}
+                </div>
+              </div>
+          }
+          { useFullLinks &&
+              <div className="btn-group">
+                <button type="button" className="btn btn-outline-primary dropdown-toggle"  data-toggle="dropdown">Usefull Links</button>
+                <div className="dropdown-menu">
+                  {useFullLinks}
+                </div>
+              </div>
+          }
+          { attachments &&
+              <div className="btn-group">
+                <button type="button" className="btn btn-outline-primary dropdown-toggle"  data-toggle="dropdown">Attachments</button>
+                <div className="dropdown-menu">
+                  {attachments}
+                </div>
+              </div>
+          }
         </div>
     );
 }
